26dc496dfa240231cfda418ce88763f3f1efbd2e
travinne <travinne.mutahi@moringaschool.com>
2025-07-16 20:42:02 +0300

    committing app created login,models,user_shema

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/farmart_db'
+    app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # Replace in production
+
+    db.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
+    CORS(app)
+
+    from app.login import auth_bp
+    app.register_blueprint(auth_bp)
+
+    return app
