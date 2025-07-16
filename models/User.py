a66c8defbcd880b784d92ae359b59ff26ed09371
Yazz <itsjustmaitai@gmail.com>
2025-07-16 21:02:23 +0300

    Set up authentication/login branch and updated User model

--- models/User.py
+++ models/User.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db 
 
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
