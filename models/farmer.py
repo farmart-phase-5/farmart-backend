76827b2123d2ec510d97e5d9b79960bbfe106eec
Yazz <itsjustmaitai@gmail.com>
2025-07-21 15:17:55 +0300

    Removed the farmer model

--- models/farmer.py
+++ models/farmer.py
@@ -1,20 +1,20 @@
-from sqlalchemy.orm import relationship
-from flask_jwt_extended import JWTManager
-from extension import db
+#from sqlalchemy.orm import relationship
+#from flask_jwt_extended import JWTManager
+#from extension import db
 
 
-class Farmer(db.Model):
-    __tablename__ = 'farmers'
+#class Farmer(db.Model):
+   # __tablename__ = 'farmers'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False, unique=True)
-    email = db.Column(db.String(120), nullable=True, unique=True)
+    #id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String(100), nullable=False)
+    #phone_number = db.Column(db.String(20), nullable=False, unique=True)
+    #email = db.Column(db.String(120), nullable=True, unique=True)
 
   
-    my_animals = db.relationship("Animals", back_populates="owner", lazy=True)
+    #my_animals = db.relationship("Animals", back_populates="owner", lazy=True)
 
 
 
-    def __repr__(self):
-        return f"<Farmer {self.name}>"
+    #def __repr__(self):
+     #   return f"<Farmer {self.name}>"
