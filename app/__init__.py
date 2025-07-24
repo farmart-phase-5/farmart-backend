f7b74d9bc67565eb5d16f55e03e09d2c5e0e3d0b
Joel Thayu <joelpeace254@gmail.com>
2025-07-24 12:14:38 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,25 +15,25 @@ def create_app():
     migrate.init_app(app, db)
     CORS(app)
 
-    # Import models
+  
     from app.models import farmer_model, animal_model, order_model
 
-    # Import Blueprints
+    
     from app.routes.farmer_routes import farmer_routes
     from app.routes.animal_routes import animal_routes
     from app.routes.order_routes import order_routes
 
-    # Register Blueprints with prefixes
+   
     app.register_blueprint(farmer_routes, url_prefix='/farmers')
     app.register_blueprint(animal_routes, url_prefix='/animals')
     app.register_blueprint(order_routes, url_prefix='/orders')
 
-    # Root route
+   
     @app.route('/')
     def home():
         return "Welcome to my Farmart server"
 
-    # Debug print
+ 
     print("Registered Routes:")
     print(app.url_map)
 
