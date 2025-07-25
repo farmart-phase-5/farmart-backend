526fe93d6acfeb143e52c657894f5905a1f2eba2
travinne <travinne.mutahi@moringaschool.com>
2025-07-25 23:18:42 +0300

    commit

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app import db
-from .farmer_model import Farmer
-from .animal_model import Animal
-from .order_model import AppOrder
-from .order_item import OrderItem
-
-__all__ = ['db', 'Farmer', 'Animal', 'AppOrder', 'OrderItem']
--- app/schema/farmer_schema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-
-class FarmerSchema(Schema):
-    id = fields.Integer(dump_only=True)
-    username = fields.String(required=True)
-    password = fields.String(required=True, load_only=True)
-    
-farmer_schema = FarmerSchema()
-farmers_schema = FarmerSchema(many=True)
