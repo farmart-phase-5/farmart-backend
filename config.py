5334380422706d230e3b711a7ffc76420a796fd9
Joel Thayu <joelpeace254@gmail.com>
2025-07-31 09:10:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,22 @@
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+bcrypt = Bcrypt()
+migrate = Migrate()
+api = Api()
+jwt = JWTManager() 
+
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'super-secret-key'
@@ -5,6 +24,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_BLACKLIST_ENABLED'] = True
     app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+    
     app.json.compact = False
 
     db.init_app(app)
